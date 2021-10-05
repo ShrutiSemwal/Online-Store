eee2d385cf8d209d62a9d054f347e89d52fbf53c
ShrutiSemwal <semwalshruti450@gmail.com>
2021-10-05 17:18:18 +0530

    designed auth and cart pages

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ const path= require('path')
 const expressLayout= require('express-ejs-layouts')
 const PORT = process.env.PORT || 3000
 
+
+
+//set Template engine
+app.use(expressLayout)
+
+app.set('views', path.join(__dirname,'/resources/views'))
+app.set('view engine', 'ejs')
+
 //Assets
 app.use(express.static('public'))
 
@@ -12,11 +20,17 @@ app.get('/', (req, res)=> {
    res.render('home')
 })
 
-//set Template engine
-app.use(expressLayout)
+app.get('/cart', (req,res)=>{
+   res.render('customers/cart')
+})
 
-app.set('views', path.join(__dirname,'/resources/views'))
-app.set('view engine', 'ejs')
+app.get('/login', (req,res)=>{
+   res.render('auth/login')
+})
+
+app.get('/register', (req,res)=>{
+   res.render('auth/register')
+})
 
 app.listen(PORT, ()=> {
     console.log(`Listening on port ${PORT}`)
